71c23f0027a7f39d68dd81ef57929d478515f4c6
Claudio Bantaloukas <rockdreamer@gmail.com>
2015-11-28 23:30:27 +0100

    fix build on older cmake

--- cmake/AC_RWLOCK.cmake
+++ cmake/AC_RWLOCK.cmake
@@ -3,12 +3,14 @@ try_compile(
 	HAVE_RWLOCK
 	${CMAKE_BINARY_DIR}/ac_rwlock_test
 	${CMAKE_CURRENT_SOURCE_DIR}/cmake/ac_rwlock.c
+	CMAKE_FLAGS ""
 )
 else()
 try_compile(
 	HAVE_RWLOCK
 	${CMAKE_BINARY_DIR}/ac_rwlock_test
 	${CMAKE_CURRENT_SOURCE_DIR}/cmake/ac_rwlock.c
+	CMAKE_FLAGS ""
 	LINK_LIBRARIES ${CMAKE_THREAD_LIBS_INIT}
 )
 endif()
